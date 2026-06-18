0a7b1dbfe42838d4148c3f15077df3549ddc8e1e
ivanoviedomarinNav <ivanoviedomarin285@gmail.com>
2026-06-17 18:26:00 -0600

    reco

--- src/main/java/com/ucr/reco/controller/HomeController.java
+++ src/main/java/com/ucr/reco/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.ucr.reco.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "forward:/index.html";
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public String home() throws IOException {
+        Resource resource = new ClassPathResource("static/index.html");
+        return new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
     }
 }
